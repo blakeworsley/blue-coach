a60994686311d6624d8b9594079584fdf37cafa8
Blake Worsley <blakeworsley@gmail.com>
2016-11-06 14:12:12 -0700

    WIP filtering users by day

--- src/components/Athlete.js
+++ src/components/Athlete.js
@@ -9,20 +9,21 @@ import * as actions from '../actions/athlete';
 
 class Athlete extends Component {
   render() {
-    let { firstName, lastName, emailAddress, teamName, feedback } = this.props;
-    const athleteFeedback = map(feedback, (data) => {
-      return <li key={data.previousDay.date}>
-        <p>Date: {data.previousDay.date}</p>
-        <p>Mental: {data.previousDay.mental}</p>
-        <p>Performance: {data.previousDay.performance}</p>
-        <p>Physical: {data.previousDay.physical}</p>
+    const { firstName, lastName, emailAddress, teamName } = this.props;
+    const { previousDay } = this.props.feedback;
+    const athleteFeedback = map(previousDay, (data) => {
+      return <li key={data.date}>
+        <p>Date: {data.date}</p>
+        <p>Mental: {data.mental}</p>
+        <p>Performance: {data.performance}</p>
+        <p>Physical: {data.physical}</p>
       </li>
     })
     return (
       <section>
         <h1>{firstName} {lastName}</h1>
         <section>
-          {/* {athleteFeedback} */}
+          {previousDay ? athleteFeedback : null}
         </section>
       </section>
     );
@@ -36,3 +37,28 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Athlete);
+
+
+
+//
+// const athleteFeedback = map(feedback, (data) => {
+//   data.map((previousDay) => {
+//     debugger;
+//     return <li key={previousDay.date}>
+//       <p>Date: {previousDay.date}</p>
+//       <p>Mental: {previousDay.mental}</p>
+//       <p>Performance: {previousDay.performance}</p>
+//       <p>Physical: {previousDay.physical}</p>
+//     </li>
+//   })
+// })
+
+// const athleteFeedback = map(feedback, (data) => {
+//   for (var i = 0; i < data.length; i++) {
+//     return <li key={data[i].date}>
+//       <p>Date: {data[i].date}</p>
+//       <p>Mental: {data[i].mental}</p>
+//       <p>Performance: {data[i].performance}</p>
+//       <p>Physical: {data[i].physical}</p>
+//     </li>
+//   }
