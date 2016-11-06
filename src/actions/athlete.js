a4400218a4ba3cf3643f1320687b1d3c66f236a1
Blake Worsley <blakeworsley@gmail.com>
2016-11-06 11:24:30 -0700

    cleaned up comments

--- src/actions/athlete.js
+++ src/actions/athlete.js
@@ -5,7 +5,6 @@ function selectedAthlete(firstName, lastName, teamName, emailAddress) {
   //TODO: Make the SSST lowercase in other app and they delete line above
   return (dispatch) => {
     firebase.database().ref(`workouts/${teamName.toLowerCase()}/${namePath}`).on('value', (snapshot) => {
-      console.log(snapshot.val());
       dispatch({
         type: 'SELECTED_ATHLETE',
         feedback: snapshot.val(),
