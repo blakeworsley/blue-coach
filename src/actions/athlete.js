66de79e98fd331e8fe3d05180f50e02cbf1a8831
Blake Worsley <blakeworsley@gmail.com>
2016-11-06 12:53:09 -0700

    data mapped through and stored on individual athlete by day week and month

--- src/actions/athlete.js
+++ src/actions/athlete.js
@@ -1,13 +1,34 @@
 import firebase from '../firebase';
+import { map } from 'lodash';
+
 
 function selectedAthlete(firstName, lastName, teamName, emailAddress) {
   const namePath = `${firstName.toLowerCase()}-${lastName.toLowerCase()}`;
-  //TODO: Make the SSST lowercase in other app and they delete line above
+
   return (dispatch) => {
     firebase.database().ref(`workouts/${teamName.toLowerCase()}/${namePath}`).on('value', (snapshot) => {
+      const millisecond = 1000;
+      const second = (millisecond * 60);
+      const minute = (second * 60);
+      const hour = (minute * 60);
+      const day = (hour * 24);
+      const week = (day * 7);
+      const month = (day * 30);
+
+      let previousDay = [];
+      let previousWeek = [];
+      let previousMonth = [];
+      let previousSeason = [];
+      let currentDate = Date.now();
+
+      map(snapshot.val(), (data) => {
+        if ((currentDate - data.date) <= month ) { previousMonth.push(data); }
+        if ((currentDate - data.date) <= week ) { previousWeek.push(data); }
+        if ((currentDate - data.date) <= day ) { return previousDay.push(data); }
+      });
       dispatch({
         type: 'SELECTED_ATHLETE',
-        feedback: snapshot.val(),
+        feedback: { previousDay, previousWeek, previousMonth, previousSeason },
         firstName: firstName,
         lastName: lastName,
         teamName: teamName,
@@ -17,7 +38,6 @@ function selectedAthlete(firstName, lastName, teamName, emailAddress) {
     });
   };
 }
-
 
 export {
   selectedAthlete
--- src/components/Athlete.js
+++ src/components/Athlete.js
@@ -11,18 +11,18 @@ class Athlete extends Component {
   render() {
     let { firstName, lastName, emailAddress, teamName, feedback } = this.props;
     const athleteFeedback = map(feedback, (data) => {
-      return <li key={data.date}>
-        <p>Date: {data.date}</p>
-        <p>Mental: {data.mental}</p>
-        <p>Performance: {data.performance}</p>
-        <p>Physical: {data.physical}</p>
+      return <li key={data.previousDay.date}>
+        <p>Date: {data.previousDay.date}</p>
+        <p>Mental: {data.previousDay.mental}</p>
+        <p>Performance: {data.previousDay.performance}</p>
+        <p>Physical: {data.previousDay.physical}</p>
       </li>
     })
     return (
       <section>
         <h1>{firstName} {lastName}</h1>
         <section>
-          {athleteFeedback}
+          {/* {athleteFeedback} */}
         </section>
       </section>
     );
