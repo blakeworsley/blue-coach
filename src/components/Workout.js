6e1adf1787e626259cab5edbd9aff04bed759ee7
Blake Worsley <blakeworsley@gmail.com>
2016-11-10 09:05:36 -0700

    corrected average in workout

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 class Workout extends Component {
+
+
   render() {
     const { date, mental, performance, physical } = this.props;
-    const averageScore = () => {
-      return Math.round((+mental + +performance + +physical) / 3);
-    };
     return (
-      <li key={date} className="workout-container">
+      <li key={date} className="workout-container three">
         <p>Date: {moment(date).format('LLL')}</p>
-        <p>Average: {averageScore}</p>
+        <p>Average: {Math.round((parseInt(mental) + parseInt(performance) + parseInt(physical)) / 3)}</p>
         <p>Mental: {mental}</p>
         <p>Performance: {performance}</p>
         <p>Physical: {physical}</p>
